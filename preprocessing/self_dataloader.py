f72f63a7285ef1f51b672bee0f1c1d3e0afff4a4
叶濬哲 <3283664815@qq.com>
2025-05-01 16:27:14 +0800

    更新了自定义数据集加载代码

--- preprocessing/self_dataloader.py
+++ preprocessing/self_dataloader.py
@@ -1,1 +1,21 @@
 ## 未来在此处添加自己的数据集
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+# 数据预处理
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),  # 调整图像大小
+    transforms.ToTensor(),  # 转换为张量
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # 归一化
+])
+
+data_dir = 'data'  # 数据集根目录
+train_dataset = datasets.ImageFolder(root=f'{data_dir}/train', transform=transform)
+test_dataset = datasets.ImageFolder(root=f'{data_dir}/test', transform=transform)
+
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+for images, labels in train_loader:
+    print(f"Images shape: {images.shape}, Labels shape: {labels.shape}")
+    break
