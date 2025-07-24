c34b8062df1cec8ffca500f29bec16aa8317e5c1
叶濬哲 <3283664815@qq.com>
2025-07-24 21:27:53 +0800

    统一数据加载器接口，同时添加一些功能

--- fl_main.py
+++ fl_main.py
@@ -20,7 +20,7 @@ from fed_baselines.server_base import FedServer
 from fed_baselines.server_fednova import FedNovaServer
 from fed_baselines.server_scaffold import ScaffoldServer
 from postprocessing.recorder import Recorder
-from preprocessing.baselines_dataloader import divide_data
+from preprocessing.fed_dataloader import divide_data
 
 json_types = (list, dict, str, int, float, bool, type(None))
 
